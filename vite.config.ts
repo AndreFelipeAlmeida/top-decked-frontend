7ebb77606231faf7d13152316df48242ab821ed3
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2026-02-01 17:27:28 -0300

    fix: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,31 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig( ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+
+  console.log(env.VITE_DEBUG)
+
+  return {
+    plugins: [react(), tailwindcss()],
+      server: {
+      ...(env.VITE_DEBUG === "true" && {
+        proxy: {
+          "/api": {
+            target: "http://localhost:8000",
+            changeOrigin: true,
+            secure: false
+          }
+        }
       }
+    )},
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     }
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  }
 })
