16349de08a4dd50109e60260ce7237cb444ea03a
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2026-02-02 19:40:52 -0300

    refact: remove backend url

--- src/adapters/api.ts
+++ src/adapters/api.ts
@@ -1,11 +1,5 @@
 import axios from "axios";
 
-const isDebug = import.meta.env.VITE_DEBUG === "true";
-
-const baseURL = isDebug
-  ? "/api"
-  : import.meta.env.VITE_API_URL;
-
 export const api = axios.create({
-  baseURL,
+  baseURL: "/api",
 });
