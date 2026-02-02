4d33aeff9856db2a88fe0ba1e728d7f5f57b72d0
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2026-02-02 16:24:34 -0300

    refact: new backend IP if not debugging

--- src/adapters/api.ts
+++ src/adapters/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-const basePath = "/api";
+const isDebug = import.meta.env.VITE_DEBUG === "true";
+
+const baseURL = isDebug
+  ? "/api"
+  : import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
-  baseURL: basePath,
-})
+  baseURL,
+});
