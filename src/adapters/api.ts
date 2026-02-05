854c444ec593675b7ea171b3d7a33f4e2e93c54a
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2026-02-05 15:58:30 -0300

    feat: redirect to login if token expired

--- src/adapters/api.ts
+++ src/adapters/api.ts
@@ -3,3 +3,21 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "/api",
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const status = error.response?.status;
+
+    if (status === 401) {
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("user");
+
+      if (window.location.pathname !== "/login") {
+        window.location.href = "/login";
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
